41863a268ce7b2a98f6a255ee24348589d5bd208
brycehwang <brycehwang@users.noreply.github.com>
2015-11-27 10:59:10 -0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,4 +1,9 @@
 import math
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+from igraph import *
 
 def main():
 	#inputfile = sys.argv[1]
@@ -12,12 +17,16 @@ def main():
 	#print seq_array[0]
 	#print checklengths(seq_dict)
 	if checklengths(seq_dict):
-		distmatrix = kimuradistmatrix(seq_array)
+		distmatrix = createdistmatrix(seq_array)
+		distgraph = Adjacency(distmatrix)
+		mst = spanning_tree(distgraph, return_tree = True)
+
 		#ultra_distmatrix = ultrametrify(distmatrix)
 	else:
 		print "sequence not all the same length"
 
-def kimuradistmatrix(seq_array):
+
+def createdistmatrix(seq_array):
 	distarray = []
 	for seq1_num in range(0, len(seq_array)):
 		seq_dist = [0 for x in range(0, len(seq_array))]
@@ -26,34 +35,79 @@ def kimuradistmatrix(seq_array):
 		for seq2_num in range(0, len(seq_array)):
 			seq1 = seq_array[seq1_num][1]
 			seq2 = seq_array[seq2_num][1]
-			transitions = 0
-			transversions = 0
-			for base in range(0, len(seq1)):
-				if seq1[base] != seq2[base]:
-					if (seq1[base] == "A" and seq2[base] == "G") or (seq1[base] == "G" and seq2[base] == "A") or (seq1[base] == "C" and seq2[base] == "T") or (seq1[base] == "T" and seq2[base] == "C"):
-						transitions += 1
-					else:
-						transversions += 1
-			P = float(transitions)/float(len(seq1))
-			Q = float(transversions)/float(len(seq1))
-			if (1 - 2 * P - Q) == 0:
-				seq_dist[seq2_num] = 0
-			else:
-				dist = -0.5 * math.log((1 - 2* P - Q) * math.sqrt(1 - 2 * Q))
-				print dist
-				seq_dist[seq2_num] = dist
+			seq_dist[seq2_num] = tamuradist(seq1, seq2)
 			#seq_dist[seq2_num] = 5
 		distarray.append(seq_dist)
-	print distarray[0][0]
+	#print distarray[0]
 	return distarray
 
-		
+#jukes cantor distance of two sequences
+def jcdist(seq1, seq2):
+	substitutions = 0
+	for base in range(0, len(seq1)):
+		if seq1[base] != seq2[base]:
+			substitutions += 1
+	P = float(substitutions)/float(len(seq1))
+	dist = -0.75 * math.log(1 - 4.0 / 3 * P)
+	return dist
+
+#kimura 2 parameter distance of two sequences
+def kimuradist(seq1, seq2):
+	transitions = 0
+	transversions = 0
+	for base in range(0, len(seq1)):
+		if seq1[base] != seq2[base]:
+			if (seq1[base] == "a" and seq2[base] == "g") or (seq1[base] == "g" and seq2[base] == "a") or (seq1[base] == "c" and seq2[base] == "t") or (seq1[base] == "t" and seq2[base] == "c"):
+				transitions += 1
+			else:
+				transversions += 1
+	P = float(transitions)/float(len(seq1))
+	Q = float(transversions)/float(len(seq1))
+	dist = 0
+	if (1 - 2 * P - Q) == 0:
+		dist = 0
+	else:
+		dist = -0.5 * math.log((1 - 2* P - Q) * math.sqrt(1 - 2 * Q))
+	return dist
+
+#tamura distances accounts for different nucleotide compositions too
+def tamuradist(seq1, seq2):
+	transitions = 0
+	transversions = 0
+	seq1gc = 0
+	seq2gc = 0
+	for base in range(0, len(seq1)):
+		if seq1[base] == "g" or seq1[base] == "c":
+			seq1gc += 1
+		if seq2[base] == "g" or seq2[base] == "c":
+			seq2gc += 1
+		if seq1[base] != seq2[base]:
+			if (seq1[base] == "a" and seq2[base] == "g") or (seq1[base] == "g" and seq2[base] == "a") or (seq1[base] == "c" and seq2[base] == "t") or (seq1[base] == "t" and seq2[base] == "c"):
+				transitions += 1
+			else:
+				transversions += 1
+	P = float(transitions)/float(len(seq1))
+	Q = float(transversions)/float(len(seq1))
+	theta1 = float(seq1gc) / len(seq1)
+	theta2 = float(seq2gc) / len(seq2)
+	C = theta1 + theta2 - 2 * theta1 * theta2 
+	dist = 0
+	if C == 0 or (1 - (P)/(C) - Q) == 0 or 1- 2* Q == 0:
+		dist = 0
+	else:
+		dist = -C * math.log(1 - (P)/(C) - Q) - 0.5 * (1 - C) * math.log(1 - 2* Q) 
+	return dist
+
+
+
 
 def ultrametrify(distmatrix):
-	mst = prims(distmatrix)
+	#for
+	pass 
 
-def prims(distmatrix):
-	pass
+
+	
+
 
 def checklengths(seq_dict):
 	allsamelength = True
